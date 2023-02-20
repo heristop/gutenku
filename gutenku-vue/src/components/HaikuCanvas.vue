1e44c6063a57952a0ec01fc92239131bbefefa67
Alexandre Mogère <heristop@yahoo.fr>
2023-02-21 00:37:23 +0100

    [Canvas] Custom color backgrounds

--- gutenku-vue/src/components/HaikuCanvas.vue
+++ gutenku-vue/src/components/HaikuCanvas.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { computed } from 'vue';
-import { useHaikuStore } from '../store/haiku'
-import { storeToRefs } from 'pinia'
+import { useHaikuStore } from '../store/haiku';
+import { storeToRefs } from 'pinia';
 
 const { haiku, loading } = storeToRefs(useHaikuStore())
 
@@ -12,31 +12,44 @@ const haikuImage = computed(() => {
 
     return `data:image/png;base64,${haiku.value.image}`;
 });
+
+const downloadImage = () => {
+    const imageData = haikuImage.value as string;
+    const downloadLink = document.createElement('a');
+    const bookTitle = haiku.value.book.title;
+    const chapterTitle = haiku.value.chapter.title;
+
+    downloadLink.href = imageData;
+    downloadLink.download = `${bookTitle}_${chapterTitle}`
+        .toLowerCase()
+        .replace(/[ ;.,]/g, '_');
+    downloadLink.target = '_blank';
+    downloadLink.click();
+}
 </script>
 
 <template>
   <v-card
     v-if="haiku"
     class="mx-auto pa-10 mb-6 justify-center order-last"
-    :title="$t('titleCanvasCard')"
     :loading="loading"
   >
     <img
-      alt="canvas"
+      @contextmenu.prevent
+      alt="Haiku Generated"
       :src="haikuImage"
-      download="some_name.png"
       height="300"
       aspectRatio="1"
     >
 
     <v-card-actions class="justify-end">
       <v-btn
-        data-cy="send-btn"
-        disabled
+        @click="downloadImage"
+        data-cy="download-btn"
         class="ms-2"
-        prepend-icon="mdi-instagram"
+        prepend-icon="mdi-download"
       >
-        {{ $t('btnSendInstagram') }}
+        {{ $t('btnDownload') }}
       </v-btn>
     </v-card-actions>
   </v-card>
