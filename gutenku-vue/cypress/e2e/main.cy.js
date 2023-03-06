28984e45995b2421a467bdba413ce5477fc4ab14
Alexandre Mogère <heristop@yahoo.fr>
2023-03-06 23:32:44 +0100

    [Ci] Added Cypress tests

--- gutenku-vue/cypress/e2e/main.cy.js
+++ gutenku-vue/cypress/e2e/main.cy.js
@@ -3,18 +3,35 @@
 describe('Landing page test', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000');
+        cy.intercept('POST', /\/graphql$/, { fixture: 'haiku.json' }).as('api');
     });
 
-    it('works', () => {
-        cy.intercept('POST', /\/graphsql$/, (req) => {
-            req.reply({
-                statusCode: 200, // default
-                fixture: 'haiku.json'
-            })
-        }).as('api');
+    it('homepage', () => {
+        cy.get('[data-cy=switch-api-btn]').should('be.visible');
+        cy.get('[data-cy=fetch-btn]').should('be.visible');
+        cy.get('[data-cy=copy-btn]').should('be.visible');
 
         cy.wait('@api').then(() => {
-            cy.get('[data-cy=fetch-btn').should('be.visible').click();
+            cy.get('[data-cy=fetch-btn').click();
+
+            cy.wait('@api').then(() => {
+                cy.get('[data-cy=copy-btn]').click();
+                cy.get('[data-cy=copy-success-icon]').should('be.visible');
+                cy.wait(2000);
+                cy.get('[data-cy=copy-success-icon]').should('not.exist');
+                cy.get('[data-cy=light-toggle-btn]').should('be.visible').click();
+                cy.get('[data-cy=download-btn]').should('be.visible').click();
+                cy.get('[data-cy=🤖-card]').should('not.exist');
+            });
+        });
+    });
+
+    it('ai-generation', () => {
+        cy.get('[data-cy=switch-api-btn]').click();
+        cy.get('[data-cy=fetch-btn]').should('be.visible');
+
+        cy.wait('@api').then(() => {
+            cy.get('[data-cy=🤖-card]').should('be.visible');
         });
     });
 });
--- gutenku-vue/src/components/HaikuAi.vue
+++ gutenku-vue/src/components/HaikuAi.vue
@@ -2,15 +2,16 @@
 import { useHaikuStore } from '../store/haiku';
 import { storeToRefs } from 'pinia';
 
-const { haiku, loading } = storeToRefs(useHaikuStore());
+const { haiku, loading, useAI } = storeToRefs(useHaikuStore());
 </script>
 
 <template>
   <v-card
-    v-if="haiku && haiku.description"
+    v-if="haiku && haiku.description && useAI"
+    :loading="loading"
     class="mx-auto pa-10 mb-6 justify-center"
     title="🤖"
-    :loading="loading"
+    data-cy="🤖-card"
     color="primary"
   >
     <p>
