5b98582a5f6ee8fcba6f639e3a0f37cf26209204
Alexandre Mogère <heristop@yahoo.fr>
2023-03-11 15:11:03 +0100

    [Design] Update 🖌️

--- gutenku-vue/src/views/Home.vue
+++ gutenku-vue/src/views/Home.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
 import { onMounted, computed } from 'vue';
+import AppFooter from '@/components/AppFooter.vue';
 import HaikuAi from '@/components/HaikuAi.vue';
 import HaikuCanvas from '@/components/HaikuCanvas.vue';
 import HaikuCard from '@/components/HaikuCard.vue';
 import HaikuChapter from '@/components/HaikuChapter.vue';
-import Loading from '@/components/Loading.vue';
+import SocialNetwok from '@/components/SocialNetwork.vue';
+import AppLoading from '@/components/AppLoading.vue';
 import { useHaikuStore } from '../store/haiku';
 import { storeToRefs } from 'pinia';
 
@@ -19,69 +21,48 @@ onMounted(fetchText);
 </script>
 
 <template>
-    <v-container class="fill-height">
-        <v-responsive class="d-flex text-center fill-height">
-            <loading v-if="false === firstLoaded" />
+  <v-container class="fill-height">
+    <v-responsive class="d-flex text-center fill-height">
+      <v-sheet v-if="false === firstLoaded">
+        <app-loading text="Generating Haiku..." />
+      </v-sheet>
 
-            <v-container v-if="firstLoaded">
-                <v-row>
-                    <v-col cols="12" sm="8" class="mx-auto h-100 align-center justify-center">
-                        <haiku-card />
+      <v-container v-if="firstLoaded">
+        <v-row>
+          <v-col
+            cols="12"
+            sm="8"
+            class="mx-auto h-100 align-center justify-center"
+          >
+            <haiku-card />
 
-                        <haiku-canvas />
+            <haiku-chapter />
+          </v-col>
 
-                        <haiku-chapter />
-                    </v-col>
+          <v-col
+            cols="12"
+            sm="4"
+            class="mx-auto h-100 align-center justify-center"
+          >
+            <social-netwok />
 
-                    <v-col cols="12" sm="4" class="mx-auto h-100 align-center justify-center">
-                        <v-card class="mx-auto pa-4 mb-6" title="🌸 🗻">
-                            <p>
-                                <strong>GutenKu</strong> is a Haiku generator based on a selection of books from <br />
-                                Project Gutenberg
-                            </p>
+            <haiku-ai />
 
-                            <v-card-actions class="justify-center">
-                                <v-tooltip text="Project Gutenberg" location="bottom">
-                                    <template #activator="{ props }">
-                                        <v-btn v-bind="props" class="ms-2" icon="mdi-book-open" href="https://gutenberg.org"
-                                            target="_blank" />
-                                    </template>
-                                </v-tooltip>
+            <haiku-canvas />
 
-                                <v-tooltip text="Instagram" location="bottom">
-                                    <template #activator="{ props }">
-                                        <v-btn v-bind="props" class="ms-2" icon="mdi-instagram" variant="text"
-                                            href="https://www.instagram.com/gutenku.poem" target="_blank" />
-                                    </template>
-                                </v-tooltip>
+            <app-footer />
+          </v-col>
+        </v-row>
+      </v-container>
 
-                                <v-tooltip text="GitHub" location="bottom">
-                                    <template #activator="{ props }">
-                                        <v-btn v-bind="props" class="ms-2" icon="mdi-github"
-                                            href="https://github.com/heristop/gutenku" target="_blank" />
-                                    </template>
-                                </v-tooltip>
-                            </v-card-actions>
-                        </v-card>
-
-                        <haiku-ai />
-
-                        <v-card class="mx-auto">
-                            <v-img lazy-src="@/assets/img/duel.jpg" src="@/assets/img/duel.jpg" cover />
-
-                            <v-footer class="justify-right" color="third">
-                                <small>{{ new Date().getFullYear() }} — <strong>heristop</strong></small>
-                            </v-footer>
-                        </v-card>
-                    </v-col>
-                </v-row>
-            </v-container>
-
-            <v-snackbar v-model="networkError" :timeout="2000">
-                <v-icon>mdi-alert</v-icon> {{ error }}
-            </v-snackbar>
-        </v-responsive>
-    </v-container>
+      <v-snackbar
+        v-model="networkError"
+        :timeout="2000"
+      >
+        <v-icon>mdi-alert</v-icon> {{ error }}
+      </v-snackbar>
+    </v-responsive>
+  </v-container>
 </template>
 
 <style lang="scss">
