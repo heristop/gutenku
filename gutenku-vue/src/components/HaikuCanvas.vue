78a06b8a9e7a54dac0b56efd73d111ef2df030a6
Alexandre Mogère <heristop@yahoo.fr>
2023-03-17 13:38:48 +0100

    [Ui] Added better responsive support

--- gutenku-vue/src/components/HaikuCanvas.vue
+++ gutenku-vue/src/components/HaikuCanvas.vue
@@ -29,35 +29,13 @@ const downloadImage = () => {
 </script>
 
 <template>
-  <v-card
-    v-if="haiku"
-    class="pa-4 mb-6 align-center justify-center"
-    :loading="loading"
-  >
-    <v-sheet
-      elevation="3"
-      width="300"
-      height="300"
-    >
-      <img
-        width="300"
-        @contextmenu.prevent
-        alt="Haiku Generated"
-        aspectRatio="1"
-        :src="haikuImage"
-      >
-    </v-sheet>
+    <v-card v-if="haiku" :loading="loading" class="pa-4 mb-6 align-center justify-center" color="white">
+        <img width="300" :src="haikuImage" alt="Haiku Generated" aspectRatio="1" elevation="3" @contextmenu.prevent>
 
-    <v-card-actions class="justify-end">
-      <v-btn
-        @click="downloadImage"
-        data-cy="download-btn"
-        class="ms-2"
-        color="primary"
-        prepend-icon="mdi-download"
-      >
-        Download
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+        <v-card-actions class="justify-end">
+            <v-btn @click="downloadImage" data-cy="download-btn" class="ms-2" color="primary" prepend-icon="mdi-download">
+                Download
+            </v-btn>
+        </v-card-actions>
+    </v-card>
 </template>
--- gutenku-vue/src/components/SocialNetwork.vue
+++ gutenku-vue/src/components/SocialNetwork.vue
@@ -5,63 +5,32 @@ const GITHUB_URL = 'https://github.com/heristop/gutenku';
 </script>
 
 <template>
-  <v-card
-    class="mx-auto pa-4 mb-6"
-    title="🌸 🗻"
-  >
-    <p><strong>GutenKu</strong> is a Haiku generator based on a selection of books from</p>
+    <v-card class="mx-auto pa-4 mb-6" title="🌸 🗻">
+        <p><strong>GutenKu</strong> is a Haiku generator based on a selection of books from</p>
 
-    <p>Project Gutenberg</p>
+        <p>Project Gutenberg</p>
 
-    <v-card-actions class="justify-center">
-      <v-tooltip
-        text="Project Gutenberg"
-        location="bottom"
-      >
-        <template #activator="{ props }">
-          <v-btn
-            v-bind="props"
-            color="primary"
-            class="ms-2"
-            icon="mdi-book-open"
-            :href="GUTENBERG_URL"
-            target="_blank"
-          />
-        </template>
-      </v-tooltip>
+        <v-card-actions class="justify-center">
+            <v-tooltip text="Project Gutenberg" location="bottom">
+                <template #activator="{ props }">
+                    <v-btn v-bind="props" color="primary" class="ms-2" icon="mdi-book-open" :href="GUTENBERG_URL"
+                        target="_blank" />
+                </template>
+            </v-tooltip>
 
-      <v-tooltip
-        text="Instagram"
-        location="bottom"
-      >
-        <template #activator="{ props }">
-          <v-btn
-            v-bind="props"
-            color="primary"
-            class="ms-2"
-            icon="mdi-instagram"
-            variant="text"
-            :href="INSTAGRAM_URL"
-            target="_blank"
-          />
-        </template>
-      </v-tooltip>
+            <v-tooltip text="Instagram" location="bottom">
+                <template #activator="{ props }">
+                    <v-btn v-bind="props" color="primary" class="ms-2" icon="mdi-instagram" variant="text"
+                        :href="INSTAGRAM_URL" target="_blank" />
+                </template>
+            </v-tooltip>
 
-      <v-tooltip
-        text="GitHub"
-        location="bottom"
-      >
-        <template #activator="{ props }">
-          <v-btn
-            v-bind="props"
-            color="primary"
-            class="ms-2"
-            icon="mdi-github"
-            :href="GITHUB_URL"
-            target="_blank"
-          />
-        </template>
-      </v-tooltip>
-    </v-card-actions>
-  </v-card>
+            <v-tooltip text="GitHub" location="bottom">
+                <template #activator="{ props }">
+                    <v-btn v-bind="props" color="primary" class="ms-2" icon="mdi-github" :href="GITHUB_URL"
+                        target="_blank" />
+                </template>
+            </v-tooltip>
+        </v-card-actions>
+    </v-card>
 </template>
