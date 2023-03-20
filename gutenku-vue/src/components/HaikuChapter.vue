5d31a14cfff8cd8af7429325cb5a32011c612fd6
Alexandre Mogère <heristop@yahoo.fr>
2023-03-20 20:44:54 +0100

    [Html] Seo

--- gutenku-vue/src/components/HaikuChapter.vue
+++ gutenku-vue/src/components/HaikuChapter.vue
@@ -25,51 +25,86 @@ onMounted(() => {
 </script>
 
 <template>
-    <v-card v-if="haiku" :loading="loading" color="secondary" class="pa-4 mb-6 align-center justify-center">
-        Disclose chapter where quotes were extracted
-        <v-row>
-            <v-col>
-                <v-tooltip :disabled="loading" text="Disclose / Hide" location="bottom" aria-label="Disclose / Hide">
-                    <template #activator="{ props }">
-                        <v-btn v-bind="props" :icon="blackMarker ? 'mdi-lightbulb-on' : 'mdi-lightbulb-off'"
-                            @click="toggle()" color="primary" alt="Disclose / Hide" aria-label="Disclose / Hide"
-                            title="Disclose / Hide" data-cy="light-toggle-btn" size="small" />
-                    </template>
-                </v-tooltip>
-            </v-col>
-        </v-row>
+  <v-card
+    v-if="haiku"
+    :loading="loading"
+    color="secondary"
+    class="pa-4 mb-6 align-center justify-center"
+  >
+    Disclose chapter where quotes were extracted
+    <v-row>
+      <v-col>
+        <v-tooltip
+          :disabled="loading"
+          text="Disclose / Hide"
+          location="bottom"
+          aria-label="Disclose / Hide"
+        >
+          <template #activator="{ props }">
+            <v-btn
+              v-bind="props"
+              :icon="blackMarker ? 'mdi-lightbulb-on' : 'mdi-lightbulb-off'"
+              @click="toggle()"
+              color="primary"
+              alt="Disclose / Hide"
+              aria-label="Disclose / Hide"
+              title="Disclose / Hide"
+              data-cy="light-toggle-btn"
+              size="small"
+            />
+          </template>
+        </v-tooltip>
+      </v-col>
+    </v-row>
 
-        <v-row class="paragraphes" @click="toggle()">
-            <v-col>
-                <h3 :class="{
+    <v-row
+      class="paragraphes"
+      @click="toggle()"
+    >
+      <v-col>
+        <h3
+          :class="{
+            'dark-theme': blackMarker,
+            'light-theme': !blackMarker
+          }"
+          class="ma-4 text-h5 text-center mb-4"
+        >
+          {{ haiku.book.title }}
+        </h3>
+
+        <div
+          :class="{
+            'dark-theme': blackMarker,
+            'light-theme': !blackMarker
+          }"
+          class="text-center mb-6 author"
+        >
+          {{ haiku.book.author }}
+        </div>
+
+        <v-row class="d-flex align-center justify-center">
+          <v-col cols="auto">
+            <v-sheet
+              class="overflow-y-auto"
+              max-height="400"
+            >
+              <v-card-text>
+                <p
+                  :class="{
                     'dark-theme': blackMarker,
                     'light-theme': !blackMarker
-                }" class="ma-4 text-h5 text-center mb-4">
-                    {{ haiku.book.title }}
-                </h3>
-
-                <div :class="{
-                    'dark-theme': blackMarker,
-                    'light-theme': !blackMarker
-                }" class="text-center mb-6 author">
-                    {{ haiku.book.author }}
-                </div>
-
-                <v-row class="d-flex align-center justify-center">
-                    <v-col cols="auto">
-                        <v-sheet class="overflow-y-auto" max-height="400">
-                            <v-card-text>
-                                <p :class="{
-                                    'dark-theme': blackMarker,
-                                    'light-theme': !blackMarker
-                                }">
-                                    <high-light-text :text="haiku.chapter.content" :lines="haiku.rawVerses" />
-                                </p>
-                            </v-card-text>
-                        </v-sheet>
-                    </v-col>
-                </v-row>
-            </v-col>
+                  }"
+                >
+                  <high-light-text
+                    :text="haiku.chapter.content"
+                    :lines="haiku.rawVerses"
+                  />
+                </p>
+              </v-card-text>
+            </v-sheet>
+          </v-col>
         </v-row>
-    </v-card>
+      </v-col>
+    </v-row>
+  </v-card>
 </template>
