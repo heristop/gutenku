7828b64cf9554709921fa0e9a420c6064f91ce73
Alexandre Mogère <heristop@yahoo.fr>
2023-03-23 21:57:47 +0100

    [Env] Reverted regex rule

--- gutenku-api/services/haiku.ts
+++ gutenku-api/services/haiku.ts
@@ -238,7 +238,16 @@ export default class HaikuService implements GeneratorInterface {
         const lastWordsRegex = /(Mr|Mrs|Or|And|St)$/i;
         const specialCharsRegex = /@|[0-9]|#|\[|\|\(|\)|"|“|”|--|:|,|_|—|\+|=|{|}|\]|\*|\$|%|\r|\n|;|~|&/g;
 
-        const forbiddenWordsRegex = new RegExp(`(${process.env.FORBIDDEN_EXPRESSIONS})`, 'i');
+        const forbiddenExpressions = [
+            'Translated',
+            'Illustration',
+            'On the other hand',
+            'On the contrary',
+            'copyright',
+            'provide a copy',
+        ];
+
+        const forbiddenWordsRegex = new RegExp(`(${forbiddenExpressions.join('|')})`, 'i');
         const lostLetter = /\b[A-Z]\b$/;
 
         const regexList = [
