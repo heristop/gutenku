5b305f88a2d4f9ccce2518b491eb0f0edea86eb5
Alexandre Mogère <heristop@yahoo.fr>
2023-04-01 13:23:53 +0200

    [Footer] Added legal notice

--- gutenku-vue/src/components/AppFooter.vue
+++ gutenku-vue/src/components/AppFooter.vue
@@ -1,4 +1,8 @@
-<script lang="ts" setup />
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const legalNoticeDialog = ref(false);
+</script>
 
 <template>
   <v-card class="mx-auto order-4">
@@ -13,10 +17,52 @@
       class="justify-right"
       color="third"
     >
-      <small>
-        {{ new Date().getFullYear() }} —
-        <strong>heristop</strong>
-      </small>
+      <v-dialog
+        v-model="legalNoticeDialog"
+        activator="parent"
+        width="auto"
+      >
+        <v-sheet
+          border="md"
+          class="pa-6 text-white mx-auto"
+          color="primary"
+          max-width="400"
+        >
+          <h4 class="text-h5 font-weight-bold mb-4">
+            Legal Notice
+          </h4>
+
+          <p class="mb-8">
+            <b>Gutenku.xyz</b> is owned and operated by Alexandre Mogère. Some of the content on this website is used
+            under license from third-party sources. All such content is used in accordance with the terms of the
+            applicable
+            license agreements.
+            <br>
+            <br>
+            Thank you for visiting.
+          </p>
+
+          <v-btn
+            @click="legalNoticeDialog = false"
+            block
+            class="text-none text-black mb-4"
+            color="third"
+            size="large"
+            variant="flat"
+          >
+            Close
+          </v-btn>
+        </v-sheet>
+      </v-dialog>
+
+      <v-btn
+        @click="legalNoticeDialog"
+        color="primary"
+        size="x-small"
+        variant="text"
+      >
+        {{ new Date().getFullYear() }} —&nbsp;<strong>heristop</strong>
+      </v-btn>
     </v-footer>
   </v-card>
 </template>
