6f5c296c6970ef900ae7c7f652273d85d523df5b
Alexandre Mogère <heristop@yahoo.fr>
2023-04-07 02:10:36 +0200

    [Generator] Use Natural Language

--- gutenku-api/.eslintrc.cjs
+++ gutenku-api/.eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended'
     ],
+    ignorePatterns: ['data'],
     rules: {
         '@typescript-eslint/dot-notation': 'error',
         'indent': ["error", 4],
