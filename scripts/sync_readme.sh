8f2b76f3a4213be01e6cf5c43d0702557e057369
Alexandre Mogère <heristop@yahoo.fr>
2023-04-13 20:34:05 +0200

    Update sync_readme.sh

--- scripts/sync_readme.sh
+++ scripts/sync_readme.sh
@@ -10,5 +10,6 @@ if git status | grep -qF daily_haiku_card.jpg; then
     sed -i "s/Last Snapshot:.*/Last Snapshot: \`$(date +'%a, %d %b %Y %H:%M:%S %z')\`/" README.md
     git add assets README.md
     git commit -m "[Readme] Updated Daily Haiku Card"
+    git pull
     git push https://$TOKEN@github.com/heristop/gutenku.git
 fi
