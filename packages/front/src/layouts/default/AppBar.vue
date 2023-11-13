040e8fd2ba0094e8731bfe441a29efbd2d65834a
Alexandre Mogère <heristop@yahoo.fr>
2023-11-14 00:39:09 +0100

    [Front] Changed main font

--- packages/front/src/layouts/default/AppBar.vue
+++ packages/front/src/layouts/default/AppBar.vue
@@ -4,9 +4,9 @@
   <v-app-bar color="primary">
     <v-img
       class="logo"
-      src="@/assets/img/logo/gutenku_white.webp"
+      src="@/assets/img/logo/gutenku_white.png"
       alt="GutenKu"
-      height="32"
+      height="42"
     />
   </v-app-bar>
 </template>
