4f329180123410a373be1e35c70babe14a2f2263
Alexandre Mogère <heristop@yahoo.fr>
2023-11-15 20:05:19 +0100

    [Front] Hide navigation on first loading

--- packages/front/src/layouts/default/AppBar.vue
+++ packages/front/src/layouts/default/AppBar.vue
@@ -1,40 +1,29 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useHaikuStore } from '@/store/haiku';
 import HaikuOptions from '@/components/HaikuOptions.vue';
+
+const { firstLoaded } = storeToRefs(useHaikuStore());
 
 const drawer = ref(false);
 </script>
 
 <template>
   <v-app-bar color="primary">
-    <v-spacer class="mx-6" />
+    <v-spacer :class="firstLoaded ? 'mx-7' : 'mx-0'" />
 
-    <v-img
-      class="logo"
-      src="@/assets/img/logo/gutenku_white.png"
-      alt="GutenKu"
-      height="42"
-    />
+    <v-img class="logo" src="@/assets/img/logo/gutenku_white.png" alt="GutenKu" height="42" />
 
     <v-spacer />
 
-    <v-btn
-      icon
-      @click="drawer = !drawer"
-    >
+    <v-btn v-show="firstLoaded" icon @click="drawer = !drawer">
       <v-icon>mdi-dots-vertical</v-icon>
     </v-btn>
   </v-app-bar>
 
   <!-- Navigation Drawer qui s'ouvre depuis la droite -->
-  <v-navigation-drawer
-    v-model="drawer"
-    location="right"
-    permanent
-    color="secondary"
-    width="350"
-    class="config-drawer"
-  >
+  <v-navigation-drawer v-model="drawer" location="right" permanent color="secondary" width="350" class="config-drawer">
     <haiku-options />
   </v-navigation-drawer>
 </template>
