3808f4c36e395ee8e0eb0504e89d68b4d9b2bb87
Alexandre Mogère <heristop@yahoo.fr>
2024-05-17 21:00:16 +0200

    chore: versionning description

--- scripts/sync_readme.sh
+++ scripts/sync_readme.sh
@@ -3,13 +3,14 @@
 set -e
 
 cp packages/server/data/daily_haiku_card.jpg ./assets/img/daily_haiku_card.jpg
+cp packages/server/data/description.txt ./assets/description.txt
 
 if git status | grep -qF daily_haiku_card.jpg; then
     echo '\033[1;32mNew image found\033[0m'
     sed -i "s/t=[0-9]\+/t=$(date +%s)/" README.md
     sed -i "s/Last Snapshot:.*/Last Snapshot: \`$(date +'%a, %d %b %Y %H:%M:%S %z')\`/" README.md
 
-    description=$(cat packages/server/data/description.txt)
+    description=$(cat assets/description.txt)
     sed -i "s/“[^”]*”/“$description”/" README.md
 
     git add assets README.md
