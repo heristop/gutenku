11ffd098781e9e4bffcb3e4ac8a4c820a397004b
Alexandre Mogère <heristop@yahoo.fr>
2025-09-17 00:46:36 +0200

    feat(front): upgraded e2e tests

---
+++ packages/front/cypress/e2e/error.cy.ts
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('Error handling', () => {
+  it('shows a friendly message on max-attempts error', () => {
+    cy.intercept('POST', /\/graphql$/, {
+      fixture: 'haiku_error_max_attempts.json',
+    }).as('api');
+    cy.visitApp('/');
+
+    cy.get('[data-cy=fetch-btn]').click();
+    cy.waitForHaiku('api');
+
+    cy.get('.v-snackbar', { timeout: 4000 })
+      .should('be.visible')
+      .invoke('text')
+      .then((txt) => {
+        expect(txt).to.include('I could not find a haiku');
+      });
+  });
+});
