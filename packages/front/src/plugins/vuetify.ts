e109bd805bd2c51f67820353100a5e59ad4f733b
Alexandre Mogère <heristop@yahoo.fr>
2025-09-16 13:20:58 +0200

    chore: updated to eslint 9

--- packages/front/src/plugins/vuetify.ts
+++ packages/front/src/plugins/vuetify.ts
@@ -31,5 +31,5 @@ export default createVuetify({
     themes: {
       gutenkuTheme,
     },
-  }
+  },
 });
--- packages/server/ecosystem.config.cjs
+++ packages/server/ecosystem.config.cjs
@@ -1,8 +1,10 @@
 module.exports = {
-    apps : [{
-        name: 'server',
-        script: 'ts-node',
-        args: 'src/index.ts',
-        max_memory_restart: '400M'
-    }]
+  apps: [
+    {
+      name: 'server',
+      script: 'ts-node',
+      args: 'src/index.ts',
+      max_memory_restart: '400M',
+    },
+  ],
 };
