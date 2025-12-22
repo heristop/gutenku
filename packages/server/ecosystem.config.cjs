0063ff7442eaa491a1527dcd45d74b9dc7d9c1b0
Alexandre Mogère <heristop@yahoo.fr>
2025-12-22 20:40:43 +0100

    chore: update pm2 config

--- packages/server/ecosystem.config.cjs
+++ packages/server/ecosystem.config.cjs
@@ -2,10 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'server',
-      script: 'npm',
-      args: 'start',
+      script: 'dist/index.js',
       max_memory_restart: '400M',
-      interpreter: 'none',
       env: {
         NODE_ENV: 'production',
       },
