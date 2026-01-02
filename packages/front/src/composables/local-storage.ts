9530a1f94a15cd68e1643e5c135595350d6485b4
Alexandre Mogère <heristop@yahoo.fr>
2026-01-02 12:52:52 +0100

    refactor(front): remove old component structure

--- packages/front/src/composables/local-storage.ts
+++
@@ -1,9 +0,0 @@
-import { useLocalStorage as useVueUseLocalStorage } from '@vueuse/core';
-
-export function useExpandedState(key: string, defaultValue = true) {
-  const value = useVueUseLocalStorage(key, defaultValue);
-  const toggle = () => {
-    value.value = !value.value;
-  };
-  return { value, toggle };
-}
