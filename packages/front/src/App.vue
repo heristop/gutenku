9d9e195d6d021b64be95ed113ca05214c8578ad0
Alexandre Mogère <heristop@yahoo.fr>
2026-01-02 11:03:12 +0100

    refactor(front): update app and styles

--- packages/front/src/App.vue
+++ packages/front/src/App.vue
@@ -1,5 +1,18 @@
-<script lang="ts" setup />
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+import PwaInstallBanner from '@/components/ui/PwaInstallBanner.vue';
+
+onMounted(() => {
+  // Defer analytics to idle callback
+  if ('requestIdleCallback' in globalThis) {
+    requestIdleCallback(() => import('./analytics-setup'));
+  } else {
+    setTimeout(() => import('./analytics-setup'), 2000);
+  }
+});
+</script>
 
 <template>
   <router-view />
+  <PwaInstallBanner />
 </template>
