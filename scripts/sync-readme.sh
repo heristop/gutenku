02144f9cfe3233e424c125fc0b332c61ccbda061
Alexandre Mogère <heristop@yahoo.fr>
2026-01-17 12:40:43 +0100

    refactor(scripts): simplify sync-readme logic

--- scripts/sync-readme.sh
+++ scripts/sync-readme.sh
@@ -5,18 +5,24 @@ set -e
 cp packages/server/data/daily_haiku_card.jpg ./assets/img/daily_haiku_card.jpg
 cp packages/server/data/description.txt ./assets/description.txt
 
-if git status | grep -qF daily_haiku_card.jpg; then
-    echo '\033[1;32mNew image found\033[0m'
-    sed -i "s/t=[0-9]\+/t=$(date +%s)/" README.md
+# Update image cache buster
+sed -i "s/t=[0-9]\+/t=$(date +%s)/" README.md
 
-    description=$(cat assets/description.txt)
-    escaped_desc=$(printf '%s\n' "$description" | sed 's/[&/\]/\\&/g')
-    sed -i "s/> _\".*\"_/> _\"$escaped_desc\"_/" README.md
+# Update description
+description=$(cat assets/description.txt)
+escaped_desc=$(printf '%s\n' "$description" | sed 's/[&/\]/\\&/g')
+sed -i "s/> _\".*\"_/> _\"$escaped_desc\"_/" README.md
 
-    current_date=$(date +'%b %d, %Y')
-    sed -i "s/📅 _[^_]*_/📅 _${current_date}_/" README.md
+# Update date
+current_date=$(date +'%b %d, %Y')
+sed -i "s/📅 _[^_]*_/📅 _${current_date}_/" README.md
 
+# Commit and push if there are changes
+if ! git diff --quiet assets README.md; then
+    echo '\033[1;32mChanges detected, committing...\033[0m'
     git add assets README.md
     git commit -m "docs(readme): updated daily haiku card"
     git push https://$TOKEN@github.com/heristop/gutenku.git
+else
+    echo '\033[1;33mNo changes to commit\033[0m'
 fi
